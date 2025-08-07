5bbde36cfab01266d1ff4b146403648f6697532f
daniel.sitar <sitarpublic@gmail.com>
2025-08-07 12:25:25 +0200

    add other math func

--- calculator/index.cpp
+++ calculator/index.cpp
@@ -5,6 +5,19 @@ using namespace std;
 void Plus(int x, int y) {
   cout << "Result : " << x+y;
 }
+
+void Subtract(int x, int y){
+    cout << "Result : " << x-y;
+
+}
+void Multiply(int x, int y) {
+  cout << "Result : " << x*y;
+}
+
+void Divide(int x, int y){
+    cout << "Result : " << x/y;
+
+}
 int main() {
   Plus(5,7);
   return 0;
